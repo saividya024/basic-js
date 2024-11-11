d5c44e59cbcb4859fd243fa70d1e1efed13ead94
saividya024 <chsaividya180305@gmail.com>
2024-11-11 21:58:18 +0530

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,19 @@
+
+let n=document.getElementById("result")
+
+function validate(value){
+            n.textContent=n.textContent == "0"? value : n.textContent +value;
+}
+
+function clearResult(){
+    n.textContent=0
+    
+}
+
+function calculate(){
+       n.textContent=eval(n.textContent)
+      
+       
+}
+
+// }
